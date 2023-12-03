9498eed56b5529fc2aa718c4784d0bcc3e5893fd
juancuevas <jcuevas@globalsoft.email>
2023-12-03 17:25:37 -0600

    Añadir Highcharts

--- front/src/Admin/Estadisticas.svelte
+++ front/src/Admin/Estadisticas.svelte
@@ -1,32 +1,72 @@
 <script>
     import { onMount } from 'svelte';
-	import { isAuthenticated } from '../js/auth';
+    import { isAuthenticated } from '../js/auth';
     import { push } from 'svelte-spa-router';
+    import { getFunction } from '../js/asyncFunctions';
     import NavAdmin from '../Components/NavAdmin.svelte';
-
+    import Highcharts from 'highcharts';
+    import HighchartsExporting from 'highcharts/modules/exporting';
+  
+    let rutas = [];
+    let distancia = [];
+    let chart1;
+    let chart2;
+  
+    HighchartsExporting(Highcharts);
+  
     onMount(async () => {
-		if (!isAuthenticated() || localStorage.getItem('admin') != '1'){
+		if (!isAuthenticated() || localStorage.getItem('admin') !== '1') {
 			push('/');
 		}
+	
+		rutas = await getFunction('http://34.70.30.227:5000/graph1');
+		distancia = await getFunction('http://34.70.30.227:5000/graph2');
 
-	});
+		chart1 = Highcharts.chart('rutas', {
+			chart: {
+				type: 'column'
+			},
+			title: {
+				text: 'Distancia promedio por compañia'
+			},
+			series: [{
+				name:'ID Compañia',
+				data: rutas
+			}]
+		});
 
+		chart2 = Highcharts.chart('distancia', {
+			chart: {
+				type: 'column'
+			},
+			title: {
+				text: 'Cantidad de rutas por compañia'
+			},
+			series: [{
+				name:'ID Compañia',
+				data: distancia
+			}]
+		});
+    });
 </script>
-
+  
 <NavAdmin />
-
+  
 <div class="container mt-5 bg-white rounded shadow pt-5 pb-5 p-5 h-100 mb-5 w-100">
     <div class="container">
         <h1 class="text-center mb-5"> Estadisticas de la compañia </h1>
     </div>
 
     <div class="row ">
-        <div class="col" style="margin-left: 10rem;">
-            <img style="width: 80%;" src="http://34.170.178.166/~j18366496/mean_distance_per_company.png" alt="Distancia promedio">
-        </div>
-        <div class="col">
-            <img  style="width: 80%;"src="http://34.170.178.166/~j18366496/routes_per_company.png" alt="Cantidad de rutas por compañia">
-        </div>
-    </div>
 
+        <div class="col">
+            <div id="rutas"></div>
+        </div>
+
+        <div class="col">
+            <div id="distancia"></div>
+        </div>
+
+    </div>
 </div>
+  
---
+++ front/src/Components/Grafica.svelte
@@ -0,0 +1,41 @@
+<script>
+    import { onMount } from 'svelte';
+    import Highcharts from 'highcharts';
+    import HighchartsExporting from 'highcharts/modules/exporting';
+    
+    export let id = "";
+    export let titulo = "";
+    export let data = [];
+
+    
+    HighchartsExporting(Highcharts);
+
+    let chart; 
+
+    onMount(() => {
+        console.log(data);
+        chart = Highcharts.chart(id, 
+            {
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: titulo
+                },
+                series: [{
+                    data: data 
+                }]
+            }
+        );
+    });
+
+</script>
+
+<div id={id}></div>
+
+<style>
+  #miGrafico {
+    width: 100%;
+    height: 400px;
+  }
+</style>
