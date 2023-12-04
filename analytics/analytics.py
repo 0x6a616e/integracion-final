e7f8423adf9cf935215b7997aba49fb09931f8b1
0x6a616e <janel.fantasma2002@gmail.com>
2023-12-03 18:40:22 -0600

    feat: analytics

--- analytics/analytics.py
+++ analytics/analytics.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
 import json
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
 
 spark = SparkSession.builder.appName("analytics").getOrCreate()
 
@@ -8,12 +9,14 @@ response = urlopen("http://34.70.30.227:5000/data/truck")
 json_data = response.read().decode('utf-8', 'replace')
 
 d = json.loads(json_data)
-df = spark.read.json(d["data"])
+df = spark.createDataFrame(d["data"])
 
-df_routes_per_company = df[["company_id", "route_count"]].astype({"company_id": "string", "route_count": "int"})
-df_routes_per_company = df_routes_per_company.groupby("company_id").sum()
-df_routes_per_company.to_csv("routes_per_company.csv", sep=",", encoding="utf-8")
+df1 = df.select("company_id", "route_count")
+df1 = df1.withColumn("route_count", col("route_count").cast("Integer"))
+df1 = df1.groupby("company_id").sum("route_count")
+df1.write.mode("append").option("header", True).csv("routes_per_company")
 
-df_distance_per_company = df[["company_id", "total_distance"]].astype({"company_id": "string", "total_distance": "float"})
-df_distance_per_company = df_distance_per_company.groupby("company_id").mean()
-df_distance_per_company.to_csv("distance_per_company.csv", sep=",", encoding="utf-8")
+df2 = df[["company_id", "total_distance"]]
+df2 = df2.withColumn("total_distance", df2.total_distance.cast("float"))
+df2 = df2.groupby("company_id").mean()
+df2.write.mode("append").option("header", True).csv("distance_per_company")
